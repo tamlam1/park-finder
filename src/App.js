6f036b819e5798222111738de0dfc2496d467fb1
Tam Lam <tamhailam1@gmail.com>
2022-01-19 16:42:56 +1100

    added google map and markers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react'
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
 function App() {
+
+  const containerStyle = {
+    width: '100vw',
+    height: '100vh'
+  };
+  
+  const [center, setCenter] = React.useState({
+    lat: -33.868820,
+    lng: 151.209290
+  });
+  
+  const libraries = ['places']
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+    libraries,
+  });
+
+  const [markers, setMarkers] = React.useState([])
+
+  if (loadError) return "Error loading google maps"
+  if (!isLoaded) return "Loading google maps..."
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        zoom={10}
+        center={center}
+        
+        onClick={(e) => {
+          console.log(e)
+          setMarkers(currentMarkers => [...currentMarkers, {
+            lat: e.latLng.lat(),
+            lng: e.latLng.lng()
+          }])
+          setCenter({
+            lat: e.latLng.lat(),
+            lng: e.latLng.lng()
+          })
+        }}
+      >
+      {markers.map((marker, index) => <Marker position={{lat: marker.lat, lng: marker.lng}} key={index}/>)}
+      </GoogleMap>
     </div>
+  
   );
 }
 
